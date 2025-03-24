118faa01b5294b7e87a3492b22d24a3170719ea0
jay.yang <858851768@qq.com>
2025-03-24 08:46:28 +0800

    init: Motter with NextJs

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div className='h-full flex items-center justify-center gap-4'>
+      <Button>Welcome to Motter</Button>
+    </div>
+  );
+}
