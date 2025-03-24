ac415bbba04af725c14d40f6953a2d6939ceff92
jay.yang <858851768@qq.com>
2025-03-25 00:12:31 +0800

    chore:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { AuthScreen } from '@/feature/auth/components/AuthScreen';
 
 export default function Home() {
   return (
-    // <div className='h-full flex items-center justify-center gap-4'>
-    //   <Button>Welcome to Motter</Button>
-    // </div>
-    <AuthScreen />
+    <div className='h-full flex items-center justify-center gap-4'>
+      <Button>Welcome to Motter</Button>
+    </div>
+    // <AuthScreen />
   );
 }
