f41129e64955c785760fcd1a779d54638bea1755
jie.yang <858851768@qq.com>
2025-03-25 00:16:07 +0800

    Release/v1.0.0: add auth with convex (#1)
    * chore:
    
    * chore:
    
    * chore:
    
    * chore:
    
    * style: format
    
    * style: add .vscode folder
    
    * chore:
    
    * chore:

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,7 @@
+import { AuthScreen } from '@/feature/auth/components/AuthScreen';
+
+const AuthPage: React.FC = () => {
+  return <AuthScreen />;
+};
+
+export default AuthPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { AuthScreen } from '@/feature/auth/components/AuthScreen';
 
 export default function Home() {
   return (
     <div className='h-full flex items-center justify-center gap-4'>
       <Button>Welcome to Motter</Button>
     </div>
+    // <AuthScreen />
   );
 }
