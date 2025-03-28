62d84f62b57bf1bdaf5448f1a41282dcfab48265
jay.yang <858851768@qq.com>
2025-03-28 15:39:24 +0800

    style: DrawerContent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,12 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { useDrawer } from '@/context/DrawerContext';
 import { UserBtn } from '@/feature/auth/components/UserBtn';
 
 export default function Home() {
-  const { openDrawer } = useDrawer();
   return (
     <div className='h-full flex items-center justify-center gap-4'>
-      <Button
-        onClick={() => {
-          openDrawer({
-            id: 'welcome',
-            title: 'Welcome to Motter',
-            description: 'This is a sample drawer component.',
-            direction: 'right',
-            content: <div>Drawer content goes here.</div>,
-          });
-        }}
-      >
-        Welcome to Motter
-      </Button>
-      <Button
-        onClick={() => {
-          openDrawer({
-            id: 'welcome',
-            title: 'Welcome to Motter',
-            description: 'This is a sample drawer component.',
-            direction: 'bottom',
-            content: <div>Drawer content goes here.</div>,
-          });
-        }}
-      >
-        Welcome to Motter2
-      </Button>
+      <Button>Welcome to Motter</Button>
       <UserBtn />
     </div>
   );
--- src/context/DrawerContext.tsx
+++ src/context/DrawerContext.tsx
@@ -4,15 +4,6 @@ import { createContext, ReactNode, useContext, useReducer } from 'react';
 
 import { Button } from '@/components/ui/button';
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-} from '@/components/ui/drawer';
-import {
   Sheet,
   SheetClose,
   SheetContent,
@@ -96,61 +87,9 @@ export const DrawerContextProvider = ({ children }: { children: ReactNode }) => 
           drawerProps.onClose?.(); // Call the onClose handler if provided
         };
 
-        // use `Drawer` when the direction is 'bottom', and use `Sheet` when the direction is 'right'
-        const DrawerComponent = (
-          <Drawer
-            key={id}
-            open={isOpen}
-            direction={'bottom'}
-            onOpenChange={onCloseHandler}
-            onAnimationEnd={() => {
-              if (!isOpen) {
-                dispatch({ type: 'REMOVE', payload: { id: id as DrawerId } });
-              }
-            }}
-          >
-            <DrawerContent>
-              <div className='mx-auto w-full max-w-sm'>
-                <DrawerHeader>
-                  <DrawerTitle>{drawerProps.title}</DrawerTitle>
-                  {drawerProps.description && (
-                    <DrawerDescription>{drawerProps.description}</DrawerDescription>
-                  )}
-                </DrawerHeader>
-                <div className='p-4 pb-0'>
-                  <div className='flex items-center justify-center space-x-2'>
-                    {drawerProps?.content}
-                  </div>
-                </div>
-                {(drawerProps?.showCloseButton || drawerProps?.showConfirmButton) && (
-                  <DrawerFooter>
-                    {drawerProps?.showConfirmButton && (
-                      <Button
-                        onClick={() => {
-                          drawerProps.onConfirm?.();
-                          closeDrawer(id as DrawerId);
-                        }}
-                      >
-                        {drawerProps?.confirmText || 'Confirm'}
-                      </Button>
-                    )}
-                    {drawerProps?.showCloseButton && (
-                      <DrawerClose asChild>
-                        <Button variant='outline' onClick={onCloseHandler}>
-                          {drawerProps?.cancelText || 'Cancel'}
-                        </Button>
-                      </DrawerClose>
-                    )}
-                  </DrawerFooter>
-                )}
-              </div>
-            </DrawerContent>
-          </Drawer>
-        );
-
         const SheetComponent = (
           <Sheet key={id} open={isOpen} onOpenChange={onCloseHandler}>
-            <SheetContent side={'bottom'}>
+            <SheetContent side={drawerProps?.direction || 'right'}>
               <SheetHeader>
                 <SheetTitle>{drawerProps.title}</SheetTitle>
                 {drawerProps?.description && (
@@ -170,8 +109,7 @@ export const DrawerContextProvider = ({ children }: { children: ReactNode }) => 
             </SheetContent>
           </Sheet>
         );
-
-        return drawerProps?.direction === 'right' ? SheetComponent : DrawerComponent;
+        return SheetComponent;
       })}
     </DrawerContext.Provider>
   );
