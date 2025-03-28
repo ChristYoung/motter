b6cfa6fe596f091c24e24b8d6980e9948b69f740
jay.yang <858851768@qq.com>
2025-03-28 13:40:37 +0800

    chore:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button';
+import { UserBtn } from '@/feature/auth/components/UserBtn';
 
 export default function Home() {
   return (
     <div className='h-full flex items-center justify-center gap-4'>
       <Button>Welcome to Motter</Button>
+      <UserBtn />
     </div>
   );
 }
