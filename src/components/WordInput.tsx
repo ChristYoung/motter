5241a83b07df3d0ecf4a9921f60d623306e9ee18
jay.yang <858851768@qq.com>
2025-04-04 13:03:15 +0800

    chore:

--- src/components/WordInput.tsx
+++ src/components/WordInput.tsx
@@ -1,5 +1,24 @@
 'use client';
 
+import { useState } from 'react';
+
+import { Input } from '@/components/ui/input';
+
 export const WordInput: React.FC = () => {
-  return <div className='__WordInput'>WordInput component works!</div>;
+  const [wordQueue, setWordQueue] = useState<string[]>(['']);
+  return (
+    <div className='__WordInput'>
+      {wordQueue.map((word, index) => (
+        <Input
+          key={index}
+          value={word}
+          onChange={(e) => {
+            const newWordQueue = [...wordQueue];
+            newWordQueue[index] = e.target.value;
+            setWordQueue(newWordQueue);
+          }}
+        />
+      ))}
+    </div>
+  );
 };
