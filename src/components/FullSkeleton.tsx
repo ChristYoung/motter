cc066fa3e48f020d4f5db2a28bd459e8c7695d84
jay.yang <858851768@qq.com>
2025-04-05 13:11:47 +0800

    style:

--- src/components/FullSkeleton.tsx
+++ src/components/FullSkeleton.tsx
@@ -2,15 +2,15 @@ import { Skeleton } from './ui/skeleton';
 
 export const FullSkeleton: React.FC = () => {
   return (
-    <div className='flex items-center space-x-4'>
+    <div className='w-full flex items-center space-x-4'>
       <Skeleton className='h-12 w-12 rounded-full' />
-      <div className='space-y-2'>
-        <Skeleton className='h-4 w-[250px]' />
-        <Skeleton className='h-4 w-[200px]' />
-        <Skeleton className='h-4 w-[250px]' />
-        <Skeleton className='h-4 w-[200px]' />
-        <Skeleton className='h-4 w-[250px]' />
-        <Skeleton className='h-4 w-[200px]' />
+      <div className='w-full space-y-2'>
+        <Skeleton className='h-4 w-full' />
+        <Skeleton className='h-4 w-full' />
+        <Skeleton className='h-4 w-full' />
+        <Skeleton className='h-4 w-full' />
+        <Skeleton className='h-4 w-full' />
+        <Skeleton className='h-4 w-full' />
       </div>
     </div>
   );
